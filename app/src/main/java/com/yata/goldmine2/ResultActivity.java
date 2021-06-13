d259dc83b31c51338ee43b33fd777d8f9dc3b313
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2021-06-13 09:25:41 +0700

    them hinh anh

--- app/src/main/java/com/yata/goldmine2/ResultActivity.java
+++ app/src/main/java/com/yata/goldmine2/ResultActivity.java
@@ -30,6 +30,7 @@ public class ResultActivity extends AppCompatActivity {
                     .putInt("save_mine_count_2", 0)
                     .putInt("gold_count", 0)
                     .commit();
+            // them hinh anh
             startActivity(intent1);
             finish();
         });
